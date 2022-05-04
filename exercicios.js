195e8136a59487184566853b1575009049f274f9
Xablau <102757880+iTzXablau123@users.noreply.github.com>
2022-05-04 15:26:35 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,9 @@
+//1. Converta as seguintes expressões matemáticas para que possam ser calculadas usando Javascript: 
+
+console.log(10+20*30)
+console.log(4**2/30)
+console.log((9**4+2)*6-1)
+
+//2. Digite as seguintes expressões no Javascript:
+
+console.log(10%3*10**1+1-10*4/2)
